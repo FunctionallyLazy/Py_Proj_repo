be978d1c2a3eda36e114f9ff6bc50713442726b6
Scott <hehemaj0271@gmail.com>
2024-11-10 18:02:55 -0500

    Final Submission

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Py_Proj_repo
- Sprint 4 Intermediate Python Project
+Sprint 4 Intermediate Python Project
+
+- This repo's primary function is writing to render. A framework for easily setting up web apps. The specific use case for this is to analyze a dataset of Vehicles and their various properties. 
+
+- Dataset is loaded using os library to not have to worry about where the file is located or any potential issues locating it. 
+
+- Missing and duplicated values have been checked for and dealt with accordingly. 
+
+- Multiple visualization types have been shown for a basic exploratory data analysis. 
+
+- Some of these visualtions have been integrated with the framework's various input methods to allow user interactivity. 
--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import os
 import pandas as pd
 import plotly.express as px
 
-# df = pd.read_csv("vehicles_us.csv")
-
+# Was having issues just passing "vehicles_us.csv" to read_csv method along with other variants. 
+# This was the only method that worked locally and when pushing to render
 file_path = os.path.join(os.path.dirname(__file__), "vehicles_us.csv")
 df = pd.read_csv(file_path)
 
@@ -14,7 +14,8 @@ df = pd.read_csv(file_path)
 def get_manufact(model):
     return model.split(' ')[:1][0]
 
-st.header('Project')
+st.header('Sprint 4 Project - Vehicle Dataset Analysis')
+
 
 # Cleaning the dataset. Dropped is_4wd column has ~1/2 of it was missing. Converted dtype of columns
 # to correct ones as well. 
@@ -25,17 +26,21 @@ df['odometer'] = df['odometer'].where(df['odometer'].isna() == False, df['odomet
 df[['model_year', 'cylinders', 'odometer']] = df[['model_year', 'cylinders', 'odometer']].astype('int')
 df['date_posted'] = pd.to_datetime(df['date_posted'], format='%Y-%m-%d')
 
+st.write('Sample of the dataset')
 st.write(df.sample(15))
 
 # Hist of Condition vs Model year measured by the avg prices 
+st.write('Condition vs Model year by average prices')
 st.write(px.histogram(df, x='model_year', y='price', histfunc='avg', color='condition', marginal='rug', hover_data=df.columns))
 
 # Creating the manufacterer column referencing the above function get_manufact() 
 # then creating a hist of the types per manufacterer
+st.write('Vehicle type count per manufacterer')
 df['manufacterer'] = df['model'].apply(get_manufact)
 st.write(px.histogram(df, x='manufacterer', color='type', marginal='rug', hover_data=df.columns))
 
-# Scatterplot of odometer and price, no correlation. Slider to set max odometer value (in thousandths)
+# Scatterplot of odometer and price. Slider to set max odometer value (in thousandths)
+st.write('Correlation between Odometer and Price')
 scatter_x_max = st.slider('Max odometer value (thousands)', 1,1000, 500)
 fig = px.scatter(df, x='odometer', y='price', color='condition')
 fig.update_layout(xaxis=dict(range=[0,scatter_x_max*1000]))
@@ -62,7 +67,9 @@ else:
 
 # Filtering initial df into seperate df with only the selected manufacterers 
 filtered_df = df.query("manufacterer == @man1 or manufacterer == @man2")
+st.write('Vehicle Price distribution comparison by manufacterers')
 st.write(px.histogram(filtered_df, x='price',color='manufacterer', marginal='rug', histnorm=distNormalize, hover_data=df.columns))
 
 # Boxplot of fuel x days_listed to show each fuel types quartiles and outliers 
+st.write('Fuel type across days listed')
 st.write(px.box(df, x='fuel', y='days_listed', color='fuel'))
