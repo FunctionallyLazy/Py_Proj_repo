dc4d5a3595e8e58fa132a94e51e63b6750d29693
Scott <hehemaj0271@gmail.com>
2024-11-10 13:58:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -23,15 +23,24 @@ df['date_posted'] = pd.to_datetime(df['date_posted'], format='%Y-%m-%d')
 
 st.write(df.sample(15))
 
+# Hist of Condition vs Model year measured by the avg prices 
 st.write(px.histogram(df, x='model_year', y='price', histfunc='avg', color='condition', marginal='rug', hover_data=df.columns))
 
+# Creating the manufacterer column referencing the above function get_manufact() 
+# then creating a hist of the types per manufacterer
 df['manufacterer'] = df['model'].apply(get_manufact)
 st.write(px.histogram(df, x='manufacterer', color='type', marginal='rug', hover_data=df.columns))
 
-st.write(px.scatter(df, x='odometer', y='price', color='condition'))
+# Scatterplot of odometer and price, no correlation. Slider to set max odometer value (in thousandths)
+scatter_x_max = st.slider('Max odometer value (thousands)', 1,1000, 500)
+fig = px.scatter(df, x='odometer', y='price', color='condition')
+fig.update_layout(xaxis=dict(range=[0,scatter_x_max*1000]))
+st.write(fig)
 
-
+# Alphabetically sorted list of manufacterers
 manu_list = sorted(df['manufacterer'].unique())
+
+# Dropdowns for future reference in manufacterer x price hist
 man1 = st.selectbox('Select first manufacterer',
     (manu_list)
 )
@@ -47,8 +56,9 @@ if distNormalizeAgree:
 else:
     distNormalize = None
 
-
+# Filtering initial df into seperate df with only the selected manufacterers 
 filtered_df = df.query("manufacterer == @man1 or manufacterer == @man2")
 st.write(px.histogram(filtered_df, x='price',color='manufacterer', marginal='rug', histnorm=distNormalize, hover_data=df.columns))
 
+# Boxplot of fuel x days_listed to show each fuel types quartiles and outliers 
 st.write(px.box(df, x='fuel', y='days_listed', color='fuel'))
